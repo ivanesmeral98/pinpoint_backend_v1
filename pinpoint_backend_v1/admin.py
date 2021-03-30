eeefe4796f9a614e68284f420f6890ea815cf28b
Ivan Esmeral <ivanesmeral@ivan.fios-router.home>
2021-03-29 21:18:17 -0400

    fix for admin console

--- pinpoint_backend_v1/admin.py
+++ pinpoint_backend_v1/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from pinpoint_backend_v1.models import Pin
+from pinpoint_backend_v1.models import Pin, Friend, Group
 
 # Register your models here.
 # admin.site.register(Profile)
 admin.site.register(Pin)
+admin.site.register(Friend)
+admin.site.register(Group)
 # admin.site.register(LastLogout)
